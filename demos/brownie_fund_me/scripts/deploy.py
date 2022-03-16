a95e412118e57a4423129be900e7542a7403932d
Dan Vlad <vladdan543@gmail.com>
2022-03-16 21:40:10 +0000

    fund me app to github

--- demos/brownie_fund_me/scripts/deploy.py
+++ demos/brownie_fund_me/scripts/deploy.py
@@ -1,11 +1,32 @@
-from brownie import FundMe, config, network, accounts
-from scripts.helpful_scripts import get_account
+from brownie import FundMe, MockV3Aggregator, config, network
+from scripts.helpful_scripts import (
+    deploy_mocks,
+    get_account,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
 
 
 def deploy_fund_me():
     account = get_account()
-    fund_me = FundMe.deploy({"from": account}, publish_source=True)
+    # pass the pricefeed address to the fundMe contract
+
+    # if we are on a persistent address like rinkeby, use the address
+    # else use mocks
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
     print(f"Contract deployed to {fund_me.address}")
+    return fund_me
 
 
 def main():
